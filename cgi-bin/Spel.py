584a4c4b5a241f01f029142d9d185c0d2f5111ef
nivdorpe <niels.vandorpe@ugent.be>
2020-05-06 23:42:36 +0200

    python code now dumps the corrects json gameinfofile

--- cgi-bin/Spel.py
+++ cgi-bin/Spel.py
@@ -1,12 +1,27 @@
 import random
+import string
+import json
 
 
 class Spel:
+    # representatie van het spelbord like [["Android", 'red', false], ... ]
     bord = []
+
+    # 25 random gekozen woorden
     wordlist = []
 
+    # de code nodig voor een spel te kunnen joinen
+    gamecode = ''
+
+    # bijhouden welk team (ROOD | BLAUW) er momenteel aan de beurt is
+    currentTeam = ''
+
     def __init__(self):
-        pass
+        self.createGameCode(8)
+        print(f"Het spel wordt gestart met code {self.gamecode}")
+
+    def createGameCode(self, length=8):
+        self.gamecode = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
 
     def getWordList(self):
         with open("words.txt") as file:
@@ -18,28 +33,28 @@ class Spel:
 
         return self.wordlist
 
+
     def asignColor(self):
         kansgetal = random.randint(0, 1)
         if kansgetal == 0:
-            firstColor = 'red'
-            secondColor = 'blue'
-
+            self.currentTeam = 'red'
+            firstTeam = 'red'
+            secondTeam = 'blue'
         else:
-            firstColor = 'blue'
-            secondColor = 'red'
-
-        self.getWordList()
+            self.currentTeam = 'blue'
+            firstTeam = 'blue'
+            secondTeam = 'red'
 
         # geef het startende team 9 woorden in hun kleur
         for i in range(9):
             self.bord.append(
-                [self.wordlist[i], firstColor]
+                [self.wordlist[i], firstTeam]
             )
 
         # geef het andere team 8 woorden in hun kleur
         for i in range(8, 25):
             self.bord.append(
-                [self.wordlist[i], secondColor]
+                [self.wordlist[i], secondTeam]
             )
 
         return self.bord
@@ -50,5 +65,29 @@ class Spel:
 
         for tegel in self.bord:
             tegel.append(False)
-            print(tegel)
+        return self.bord
 
+    def switchTeam(self):
+        if self.currentTeam == 'red':
+            self.currentTeam = 'blue'
+        else:
+            self.currentTeam = 'red'
+
+    def toJson(self):
+        verzenden = {
+            "code": self.gamecode,
+            "board": self.bord,
+            "current_color": self.currentTeam,
+            "winner": None
+        }
+
+        # write gamedata to GAMECODE.json file
+        with open("data/" + self.gamecode + ".json", 'w') as fileoutput:
+            json.dump(verzenden, fileoutput)
+
+
+spel = Spel()
+spel.getWordList()
+spel.asignColor()
+spel.everythingUnvisible()
+spel.toJson()
