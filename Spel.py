3cffd8f95039978923c393ae8a1d8e2759a4b817
nivdorpe <niels.vandorpe@ugent.be>
2020-05-06 19:01:54 +0200

    generate woordenlijst like this: [Android, red, False]

--- Spel.py
+++ Spel.py
@@ -1,12 +1,54 @@
 import random
 
-def getWordList():
+
+class Spel:
+    bord = []
     wordlist = []
-    with open("words.txt") as file:
-        woordendata = file.read()
-    lines = woordendata.splitlines()
 
-    for i in range(25):
-        wordlist.append(lines[random.randrange(0, len(lines))])
+    def __init__(self):
+        pass
 
-    return wordlist
+    def getWordList(self):
+        with open("words.txt") as file:
+            woordendata = file.read()
+        lines = woordendata.splitlines()
+
+        for i in range(25):
+            self.wordlist.append(lines[random.randrange(0, len(lines))])
+
+        return self.wordlist
+
+    def asignColor(self):
+        kansgetal = random.randint(0, 1)
+        if kansgetal == 0:
+            firstColor = 'red'
+            secondColor = 'blue'
+
+        else:
+            firstColor = 'blue'
+            secondColor = 'red'
+
+        self.getWordList()
+
+        # geef het startende team 9 woorden in hun kleur
+        for i in range(9):
+            self.bord.append(
+                [self.wordlist[i], firstColor]
+            )
+
+        # geef het andere team 8 woorden in hun kleur
+        for i in range(8, 25):
+            self.bord.append(
+                [self.wordlist[i], secondColor]
+            )
+
+        return self.bord
+
+    def everythingUnvisible(self):
+        self.getWordList()
+        self.asignColor()
+
+        for tegel in self.bord:
+            tegel.append(False)
+            print(tegel)
+
